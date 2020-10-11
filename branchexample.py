aa440407bcd93748ae0ef2a3ffc5bc70f3b8eed7
S Raghavan <raghavan8007@gmail.com>
2020-10-11 11:17:20 +0530

    had to workhard a lot

---
+++ branchexample.py
@@ -0,0 +1,18 @@
+def adds(x,y):
+    sums = x+y
+    print(sums)
+
+adds(4,5)
+
+
+def subtract(x,y):
+    subs = x-y
+    print(subs)
+
+subtract(10,4)
+
+def multiply(x,y):
+    mul = x*y
+    print(mul)
+
+multiply(4,6)
